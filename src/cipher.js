c20bd5c87649b27eec82a0c1ad74fb32bc2184e5
Mariajose <mariajosegomezcampo@gmail.com>
2019-08-25 19:41:41 -0400

    mis cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,47 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
+  encode: (string,offset) => {
+    /* los elementos que queremos desplazar a la derecha */
+
+   /* console.log('encode'); */
+    let newTermino='';
+    for (var i = 0; i<string.length; i++) {
+       // texto a descifrar-convertir a Mayusculas/ obtener ascii en esa posicion
+       let resultAscii= string[i].toUpperCase().charCodeAt();//obtenemos ascii;
+
+       if(resultAscii===32){
+        newTermino+=' ';
+      }else{
+        let newAscii = (resultAscii - 65 + offset) % 26 + 65;//formula cifrado cesar
+        let resultnew = String.fromCharCode(newAscii); //convierte el ascii sustituto en la nueva letra s
+        newTermino+=resultnew;
+      }
+       
+      }
+      /*console.log(newTermino);*/
+    return newTermino;
+   
+
   },
-  decode: () => {
-    /* Acá va tu código */
+  /* los elementos que queremos desplazar a la izquierda */
+  decode: (string,offset) => {
+    //console.log('decode');
+    let newTermino='';
+    for (var i =0; i<string.length; i++){
+      let resultAscii=string[i].toUpperCase().charCodeAt();
+      if(resultAscii===32){
+        newTermino+=' ';
+      }else{
+        let newAscii= (resultAscii + 65 - offset) % 26 + 65;
+        let resultnew = String.fromCharCode(newAscii);
+        newTermino += resultnew;
+       
+      }
+      
+    }
+    /*console.log(newTermino);*/
+    return newTermino;
+    
+
+    
   }
-};
+} 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,67 @@
-/*Acá va tu código*/
+function encode(){
+    //validacion de que se intoduzca un numero en desplazamiento
+    //valida que el textarea no este vacio
+    let offset= Number(document.getElementById("number").value);
+    let string= document.getElementById("textBox").value; 
+    
+        if(string===''){ 
+            console.log('el termino no puede ser vacio') 
+            return;
+        }
+    
+        if(offset===''||isNaN(offset) ){
+            console.log('debe ser un numero') 
+            return;
+        } 
+
+        if(offset<=0 ){ //valida que el offset no sea negativo o cero
+            console.log('no puede ser negativo o cero')
+            return;
+        }
+
+       console.log('datos validos',document.getElementById(""));
+       
+
+       let response = cipher.encode(string,offset);
+        document.getElementById("textBox").value= response;
+        document.getElementById("encryptedmessage").style.display = "block";
+        document.getElementById("decryptedmessage").style.display = "none";     
+    
+}
+
+function decode(){
+    let offset= Number(document.getElementById("number").value);
+    let string= document.getElementById("textBox").value;
+
+        if(string===''){ 
+            console.log('el termino no puede ser vacio') 
+            return;
+        }
+        if(offset===''||isNaN(offset) ){
+           console.log('debe ser un numero') 
+            return;
+        } 
+        if(offset<=0 ){
+           console.log('no puede ser negativo o cero') 
+            return;
+        } 
+
+       console.log('datos validos');
+       
+         let response = cipher.decode(string,offset);
+         document.getElementById("textBox").value= response;
+         document.getElementById("encryptedmessage").style.display = "none";
+         document.getElementById("decryptedmessage").style.display = "block";
+
+     console.log("event click", string, offset);
+
+
+}
+
+function resetData(){
+   console.log('clear')
+    document.getElementById("textBox").value='';
+    document.getElementById("number").value='';
+    document.getElementById("encryptedmessage").style.display = "none";
+    document.getElementById("decryptedmessage").style.display = "none";
+}
