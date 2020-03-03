ade62f1607fbf0bca6cf39a965dfc931bce23555
Mariajose <mariajosegomezcampo@gmail.com>
2020-03-02 20:51:12 -0500

    cambios para app

--- src/cipher.js
+++ src/cipher.js
@@ -1,47 +1,46 @@
-window.cipher = {
-  encode: (string,offset) => {
-    /* los elementos que queremos desplazar a la derecha */
+const encode = (offset, string) => {
+  let nuevoTexto = '';
+  for (let i = 0; i < string.length; i++) {
+    let convAscii = string.charCodeAt(i);
 
-   /* console.log('encode'); */
-    let newTermino='';
-    for (var i = 0; i<string.length; i++) {
-       // texto a descifrar-convertir a Mayusculas/ obtener ascii en esa posicion
-       let resultAscii= string[i].toUpperCase().charCodeAt();//obtenemos ascii;
-
-       if(resultAscii===32){
-        newTermino+=' ';
-      }else{
-        let newAscii = (resultAscii - 65 + offset) % 26 + 65;//formula cifrado cesar
-        let resultnew = String.fromCharCode(newAscii); //convierte el ascii sustituto en la nueva letra s
-        newTermino+=resultnew;
-      }
-       
-      }
-      /*console.log(newTermino);*/
-    return newTermino;
-   
-
-  },
-  /* los elementos que queremos desplazar a la izquierda */
-  decode: (string,offset) => {
-    //console.log('decode');
-    let newTermino='';
-    for (var i =0; i<string.length; i++){
-      let resultAscii=string[i].toUpperCase().charCodeAt();
-      if(resultAscii===32){
-        newTermino+=' ';
-      }else{
-        let newAscii= (resultAscii + 65 - offset) % 26 + 65;
-        let resultnew = String.fromCharCode(newAscii);
-        newTermino += resultnew;
-       
-      }
-      
+    if (convAscii >= 65 && convAscii <= 90) {
+      nuevoTexto += String.fromCharCode((convAscii - 65 + parseInt(offset)) % 26 + 65); // Mayus Ascii: 65 - 90
+    } else if (convAscii >= 97 && convAscii <= 122) {
+      nuevoTexto += String.fromCharCode((convAscii - 97 + parseInt(offset)) % 26 + 97); // Minus Ascii: 97 - 122
+    } else {                            
+      nuevoTexto += string[i];       // concateno los números, signos, espacios y caracteres especiales
     }
-    /*console.log(newTermino);*/
-    return newTermino;
-    
-
-    
   }
-} 
+  return nuevoTexto;
+};
+
+
+const decode = (offset, string) => {
+  let nuevoTexto = '';
+  // let key = parseInt(offset);
+  for (let i = 0; i < string.length; i++) {
+    let convAscii = string.charCodeAt(i);
+    if (convAscii >= 65 && convAscii <= 90) {
+      nuevoTexto += String.fromCharCode((convAscii - 90 - parseInt(offset)) % 26 + 90);
+    } else if (convAscii >= 97 && convAscii <= 122) {
+      nuevoTexto += String.fromCharCode((convAscii - 122 - parseInt(offset)) % 26 + 122);
+    } else {
+      nuevoTexto += string[i];
+    }
+  }
+  return nuevoTexto;
+};
+
+
+window.cipher = {    //"cipher" es el nombre del objeto, si deseo puedo CAMBIAR de nombre del objeto.
+  encode,
+  decode,
+
+  createCipherWithOffset: (offset) => ({  //el offset 
+    encode: string => encode(offset, string), // El Nombre del 1er encode puede cambiar. Le puse encode, porque así lo pide el ejercicio
+    decode: string => decode(offset, string), // El Nombre del 1er decode puede cambiar. Le puse decode, porque así lo pide el ejercicio
+  }),
+}
+
+//Probando en la consola:
+//console.log(window.cipher.encode(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZ') ==='HIJKLMNOPQRSTUVWXYZABCDEFG');
--- src/index.js
+++ src/index.js
@@ -1,67 +1,38 @@
-function encode(){
-    //validacion de que se intoduzca un numero en desplazamiento
-    //valida que el textarea no este vacio
-    let offset= Number(document.getElementById("number").value);
-    let string= document.getElementById("textBox").value; 
-    
-        if(string===''){ 
-            console.log('el termino no puede ser vacio') 
-            return;
-        }
-    
-        if(offset===''||isNaN(offset) ){
-            console.log('debe ser un numero') 
-            return;
-        } 
 
-        if(offset<=0 ){ //valida que el offset no sea negativo o cero
-            console.log('no puede ser negativo o cero')
-            return;
-        }
+// Llamo los ID de las "Cajas de textos y botones" creados en el formulario.
+let btnEncode = document.getElementById("botonE");
+let btnDecode = document.getElementById("botonD");
+let btnLimpiar = document.getElementById("botonLimpiar");
+let ingTexto = document.getElementById("ingTextoHtml");
+let salTexto = document.getElementById("salTextoHtml");
 
-       console.log('datos validos',document.getElementById(""));
-       
 
-       let response = cipher.encode(string,offset);
-        document.getElementById("textBox").value= response;
-        document.getElementById("encryptedmessage").style.display = "block";
-        document.getElementById("decryptedmessage").style.display = "none";     
-    
+//llamo al botón Encriptar
+btnEncode.addEventListener("click", Encode);
+function Encode() {
+	let llave = document.getElementById("keyHtml").value;
+	salTexto.value = cipher.createCipherWithOffset(llave).encode(ingTexto.value);    
 }
 
-function decode(){
-    let offset= Number(document.getElementById("number").value);
-    let string= document.getElementById("textBox").value;
 
-        if(string===''){ 
-            console.log('el termino no puede ser vacio') 
-            return;
-        }
-        if(offset===''||isNaN(offset) ){
-           console.log('debe ser un numero') 
-            return;
-        } 
-        if(offset<=0 ){
-           console.log('no puede ser negativo o cero') 
-            return;
-        } 
-
-       console.log('datos validos');
-       
-         let response = cipher.decode(string,offset);
-         document.getElementById("textBox").value= response;
-         document.getElementById("encryptedmessage").style.display = "none";
-         document.getElementById("decryptedmessage").style.display = "block";
-
-     console.log("event click", string, offset);
-
-
+//llamo al botón Desencriptar
+btnDecode.addEventListener("click", Decode);
+function Decode() {
+	let llave = document.getElementById("keyHtml").value;
+	salTexto.value = cipher.createCipherWithOffset(llave).decode(ingTexto.value);    
 }
 
-function resetData(){
-   console.log('clear')
-    document.getElementById("textBox").value='';
-    document.getElementById("number").value='';
-    document.getElementById("encryptedmessage").style.display = "none";
-    document.getElementById("decryptedmessage").style.display = "none";
+
+//llamo al botón Limpiar
+btnLimpiar.addEventListener("click", limpiar);  
+function limpiar() {
+	document.getElementById("ingTextoHtml").value = "";
+	document.getElementById("keyHtml").value = "";
+	document.getElementById("salTextoHtml").value = "";
 }
+
+// IMPORTANTE: 
+// document.getElementById("keyHtml")==> Captura la caja de texto
+// .value   ==> captura el valor de la caja de texto.
+// salTexto ==> es variable de salida, no tiene un valor para capturar.
+// el .value de salTexto ==> es igual al resultado que deseo mostrar
